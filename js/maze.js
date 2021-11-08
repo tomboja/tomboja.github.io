e32398607e80968104fabc1579a446e34f58a29a
Temesgen <temesgen.dessalegn@gmail.com>
2021-11-08 14:28:00 -0600

    maze problem

---
+++ js/maze.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+  let lost = false
+  let started = false
+  let end = false
+  $(".boundary").on("mouseover", function () {
+    $(".boundary").addClass("youlose")
+    lost = true
+    $("#status").text("Sorry, you lost!, Click on 'S' to restart")
+  })
+  $("#start").on("click", function () {
+    $(".boundary").removeClass("youlose")
+    if (lost) {
+      $("#status").text("Restarted!")
+    }
+    started = true
+  })
+
+  if (!lost) {
+    $("#start").on("mouseover", function () {
+      $("#end").on("mouseover", function () {
+        $("#status").text("You won!")
+        end = true
+      })
+    })
+  }
+
+  $('#maze').on('mouseleave', function () {
+    $(".boundary").addClass("youlose")
+  })
+
+});
